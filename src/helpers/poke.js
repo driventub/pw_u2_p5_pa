3f1a3a459205b5c608fb497836679d16f4ca0077
driven <aldazpaulo@gmail.com>
2024-01-17 20:38:42 -0500

    pokemon mounted

--- src/helpers/poke.js
+++ src/helpers/poke.js
@@ -1,31 +1,65 @@
 const obtenerIdPokemones = () => {
-  const poke1 = randomIntFromInterval(1,600);
-  const poke2 = randomIntFromInterval(1,600);
-  const poke3 = randomIntFromInterval(1,600);
-  const poke4 = randomIntFromInterval(1,600);
-  const arreglo = [poke1,poke2,poke3,poke4]
+  const poke1 = randomIntFromInterval(1, 600);
+  const poke2 = randomIntFromInterval(1, 600);
+  const poke3 = randomIntFromInterval(1, 600);
+  const poke4 = randomIntFromInterval(1, 600);
+
+  return [poke1, poke2, poke3, poke4];
+};
+
+const obtenerNombresPokemones = async ([p1, p2, p3, p4]) => {
+  const nombre1 = await consultarNombre(p1);
+  const nombre2 = await consultarNombre(p2);
+  const nombre3 = await consultarNombre(p3);
+  const nombre4 = await consultarNombre(p4);
+
+  
+
+  const arreglo = [
+    {
+      id: p1,
+      nombre: nombre1,
+    },
+    {
+      id: p2,
+      nombre: nombre2,
+    },
+    {
+      id: p3,
+      nombre: nombre3,
+    },
+    {
+      id: p4,
+      nombre: nombre4,
+    },
+  ];
+
   console.log(arreglo);
+  return arreglo;
+  
 };
 
-export const obtenerIdPokemonesFachada = () => {
-  obtenerIdPokemones();
+const obtenerPokemons = async () => {
+  const arregloId = obtenerIdPokemones();
+  return await obtenerNombresPokemones(arregloId);
 };
 
-const obtenerNombresPokemones = (arreglo) => {
-    const nombre1 = consumirAPI(arreglo[0])
-    const nombre2 = consumirAPI(arreglo[1])
-    const nombre3 = consumirAPI(arreglo[2])
-    const nombre4 = consumirAPI(arreglo[3])
-    
-}
-
-function consumirAPI(id){
-    return "nombre"
+async function consultarNombre(id) {
+  const data = await consumirAPI(id);
+  return data.name;
 }
 
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+const consumirAPI = async (id) => {
+  return await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((r) =>
+    r.json()
+  );
+};
 
+export const obtenerIdPokemonesFachada = async () => {
+  return await obtenerPokemons();
+};
 // export default obtenerIdPokemonesFachada;
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -9,16 +9,27 @@
 <script>
 import PokemonOptionsVue from "@/components/PokemonOptions.vue";
 import PokemonImagenVue from "@/components/PokemonImagen.vue";
-import {obtenerIdPokemonesFachada} from "../helpers/poke.js"
+import { obtenerIdPokemonesFachada } from "../helpers/poke.js";
 
-obtenerIdPokemonesFachada()
 export default {
   components: {
     PokemonOptionsVue,
     PokemonImagenVue,
   },
+  methods: {},
+  mounted() {
+    // const arreglo = obtenerIdPokemonesFachada();
+    // console.log(arreglo);
+    console.log("Se monto el componente");
+    this.cargaInicial();
+  },
+
   methods: {
-    
+    async cargaInicial() {
+      const arreglo = await obtenerIdPokemonesFachada();
+      console.log("Se monto el componente");
+      console.log(arreglo);
+    },
   },
 };
 </script>
